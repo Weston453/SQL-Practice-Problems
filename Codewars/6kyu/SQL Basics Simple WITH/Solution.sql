cdc2c99fbf5a40e879a2b2e9b6e0a5db48085c50
Weston Isheim <wisheim@gmail.com>
2024-01-29 14:35:52 -0500

    Refactored Query to improve performance

--- Codewars/6kyu/SQL Basics Simple WITH/Solution.sql
+++ Codewars/6kyu/SQL Basics Simple WITH/Solution.sql
@@ -1,12 +1,11 @@
 with special_sales as (
   select * 
-    from sales 
+    from sales
     where price > 90.00
 )
 select id, name 
   from departments
   where id in (
     select department_id 
-      from sales 
-      where price > 90.00
+      from special_sales
   );
