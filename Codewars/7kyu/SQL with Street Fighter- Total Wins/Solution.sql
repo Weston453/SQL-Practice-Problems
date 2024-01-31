7b4f7350d5f1f20ed98c669cbd602da0575e1ba6
Weston Isheim <wisheim@gmail.com>
2024-01-31 10:59:51 -0500

    Refactored 'Street Fighter' Kata and corrected logic error

--- Codewars/7kyu/SQL with Street Fighter- Total Wins/Solution.sql
+++ Codewars/7kyu/SQL with Street Fighter- Total Wins/Solution.sql
@@ -1,8 +1,6 @@
 select name, sum(won) as won, sum(lost) as lost
   from fighters f
   join winning_moves wm on wm.id = f.move_id
-  where wm.move != 'Hadoken'
-    or wm.move != 'Shouoken'
-    or wm.move != 'Kikoken'
-  group by f.id
+  where wm.move not in ('Hadoken', 'Shouoken', 'Kikoken')
+  group by name
   order by won desc limit 6;
