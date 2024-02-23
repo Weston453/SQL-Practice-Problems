c10f0fd557fea09835aee095fd0cf8414e972de3
Weston Isheim <wisheim@gmail.com>
2024-02-23 18:39:28 -0500

    Added IF statement logic

--- Codewars/7kyu/Loan Eligibility- part 1/Solution.sql
+++ Codewars/7kyu/Loan Eligibility- part 1/Solution.sql
@@ -1,3 +1,6 @@
--- Substitute with your SQL
-select c.id as customer_id
-  from customers c;
+select c.id as customer_id,
+  if (c.age > 18 && c.age < 65 && l.loan_status == 'paid', 'loan cannot be given', 'loan cannot be given') as loan_eligibility
+  from customers c
+  join loans l on l.customer_id = c.id;
+  
+
